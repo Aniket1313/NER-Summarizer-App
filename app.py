b1f6c1fc4a387aaa5ddb07aae04e73d72303a873
Aniket Naik <naikaniket26@gmail.com>
2021-01-24 18:14:51 +0530

    Adding files

---
+++ app.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 23 14:46:36 2021
+
+@author: shailesh
+"""
+
+
+# Summarizer and Named Entity Checker NLP App
+
+import streamlit as st 
+
+from gensim.summarization import summarize
+
+# Sumy Summary Pkg
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from wordcloud import WordCloud
+import spacy
+from spacy import displacy
+import en_core_web_sm
+import matplotlib.pyplot as plt
+
+#nlp = en_core_web_sm.load()
+nlp = spacy.load('en_core_web_sm')
+
+
+# Web Scraping Pkg
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+
+# Function for Sumy Summarization
+def sumy_summarizer(docx):
+	parser = PlaintextParser.from_string(docx,Tokenizer("english"))
+	lex_summarizer = LexRankSummarizer()
+	summary = lex_summarizer(parser.document,3)
+	summary_list = [str(sentence) for sentence in summary]
+	result = ' '.join(summary_list)
+	return result
+
+
+# Fetch Text From Url
+@st.cache
+def get_text(raw_url):
+	page = urlopen(raw_url)
+	soup = BeautifulSoup(page)
+	fetched_text = ' '.join(map(lambda p:p.text,soup.find_all('p')))
+	return fetched_text
+
+
+#@st.cache(allow_output_mutation=True)
+def analyze_text(text):
+	return nlp(text)
+
+#MainFunction
+    
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+def main():
+	"""Summaryzer Streamlit App"""
+
+	st.title("Summaryzer and Entity Checker")
+
+	activities = ["Summarize","NER Checker","NER For URL"]
+	choice = st.sidebar.selectbox("Select Activity",activities)
+
+	if choice == 'Summarize':
+		st.subheader("Summarize Document")
+		raw_text = st.text_area("Enter Text Here","Type Here")
+		summarizer_type = st.selectbox("Summarizer Type",["Gensim","Sumy Lex Rank"])
+		if st.button("Summarize"):
+			if summarizer_type == "Gensim":
+				summary_result = summarize(raw_text)
+			elif summarizer_type == "Sumy Lex Rank":
+				summary_result = sumy_summarizer(raw_text)
+
+			st.write(summary_result)
+
+
+
+	if choice == 'NER Checker':
+		st.subheader("Named Entity Recog with Spacy")
+		raw_text = st.text_area("Enter Text Here","Type Here")
+		if st.button("Analyze"):
+			docx = analyze_text(raw_text)
+			html = displacy.render(docx,style="ent")
+			html = html.replace("\n\n","\n")
+			st.write(HTML_WRAPPER.format(html),unsafe_allow_html=True)
+     
+	#if choice == 'Wordcloud':
+        
+		if st.checkbox("Show WordCloud"):
+            
+			wordcloud =  WordCloud().generate(raw_text)
+			plt.imshow(wordcloud,interpolation='bilinear')
+			plt.axis("off")
+			st.pyplot()
+            #fig, ax = wordcloud
+            #st.pyplot(fig)
+        
+            #.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+
+	if choice == 'NER For URL':
+		st.subheader("Analysis on Text From URL")
+		raw_url = st.text_input("Enter URL Here","Type here")
+		text_preview_length = st.slider("Length to Preview",50,100)
+		if st.button("Analyze"):
+			if raw_url != "Type here":
+				result = get_text(raw_url)
+				len_of_full_text = len(result)
+				len_of_short_text = round(len(result)/text_preview_length)
+				st.success("Length of Full Text::{}".format(len_of_full_text))
+				st.success("Length of Short Text::{}".format(len_of_short_text))
+				st.info(result[:len_of_short_text])
+				summarized_docx = sumy_summarizer(result)
+				docx = analyze_text(summarized_docx)
+				html = displacy.render(docx,style="ent")
+				html = html.replace("\n\n","\n")
+				st.write(HTML_WRAPPER.format(html),unsafe_allow_html=True)
+				
+HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
+		
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
